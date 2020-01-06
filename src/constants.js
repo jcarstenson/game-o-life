208d383dd0cb6831017dffc908d4ffc443bdbd03
Jacob Carstenson <jacob.carstenson@headspring.com>
2020-01-06 15:00:54 -0600

    Increase board dimensions and decrease loop wait time to compensate for extra computing time

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const NUM_ROWS = 20;
-export const NUM_COLS = 20;
+export const NUM_ROWS = 64;
+export const NUM_COLS = 64;
 
 export const TOGGLE_CELL = 'TOGGLE_CELL';
 export const STEP_GENERATION = 'STEP_GENERATION';
