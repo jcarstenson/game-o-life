cfa0c9ac89fcd714564436ba5d419cb1d9222a04
Jacob Carstenson <jacob.carstenson@headspring.com>
2020-01-09 15:38:25 -0600

    Add saving and loading state to localStorage. This is throttled using lodash to one second

--- src/store.js
+++ src/store.js
@@ -1,9 +1,23 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import throttle from 'lodash.throttle';
 import rootReducer from './reducers';
 import initialState from './initialState';
+import { loadState, saveState } from './localStorage';
+
 
 const middleware = [thunk];
 
+let startingState = initialState;
+const persistedState = loadState();
+
+if (persistedState !== undefined) {
+  startingState = Object.assign({}, initialState, persistedState);
+}
+
 // eslint-disable-next-line no-underscore-dangle
-export default applyMiddleware(...middleware)(createStore)(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f);
+const store = applyMiddleware(...middleware)(createStore)(rootReducer, startingState, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f);
+
+store.subscribe(throttle(() => saveState(store.getState()), 1000));
+
+export default store;
